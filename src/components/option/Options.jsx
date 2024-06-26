098f0cea6f44e98c30778ee211bd5a1a3e75ab44
Salman-S7 <sammyzed1505@gmail.com>
2024-06-26 19:58:29 +0530

    added interactivity to header and Options

--- src/components/option/Options.jsx
+++ src/components/option/Options.jsx
@@ -1,23 +1,37 @@
+import { useState } from 'react'
 import './options.css'
 const Options = () => {
+    const [selectedOption, setSelectedOption] = useState('prv')
   return (
-    <div className='options'>
-          <ul>
-              <li>
-                  Job preview
-              </li>
-              <li>
-                  Applicants
-              </li>
-              <li>
-                  Match
-              </li>
-              <li>
-                  Messages
-              </li>
-          </ul>      
+    <div className="options">
+      <ul>
+        <li
+          onClick={() => setSelectedOption("prv")}
+          className={selectedOption === "prv" ? "option-active" : ""}
+        >
+          Job preview
+        </li>
+        <li
+          onClick={() => setSelectedOption("apll")}
+          className={selectedOption === "apll" ? "option-active" : ""}
+        >
+          Applicants
+        </li>
+        <li
+          onClick={() => setSelectedOption("match")}
+          className={selectedOption === "match" ? "option-active" : ""}
+        >
+          Match
+        </li>
+        <li
+          onClick={() => setSelectedOption("msgs")}
+          className={selectedOption === "msgs" ? "option-active" : ""}
+        >
+          Messages
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Options
